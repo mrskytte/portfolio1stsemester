0b075573a9fa5af198fc1230151d274db49b208e
Christoffer Skytte Wielsøe <FatherofPiper@Marys-MacBook-Pro.local>
2019-06-19 03:50:04 +0200

    Final tweeks - js for responsive nav bar added to all scrips

--- bw-script.js
+++ bw-script.js
@@ -3,14 +3,36 @@
 //HAMBURGER MENU
 
 const hamburger = document.getElementById("hide-button")
+const hamLine = document.getElementsByClassName("hamburger")
 const side = document.getElementsByTagName("side")
+
 
 hamburger.addEventListener("click", openMenu)
 
+var counter = 0
+
 function openMenu(){
+    if (counter == 0){
     side[0].style.top = "0";
-    hamburger.style.bottom = "107%"
+    hamburger.style.bottom = "93%";
+    hamburger.style.borderRadius = "50px"
+    hamburger.style.boxShadow = "5px 10px 20px rgba(0, 0, 0, 0.16)"
+    hamLine[0].style.opacity = "0"
+    hamLine[2].style.opacity = "0"
+    hamLine[1].style.transform = "rotate(90deg)";
+        counter++;
+}else {
+    side[0].style.top = "100vh";
+    hamburger.style.bottom = "0";
+    hamburger.style.borderRadius = ""
+    hamburger.style.boxShadow = "5px 0px 6px rgba(0, 0, 0, 0.16)"
+     hamLine[0].style.opacity = "1"
+    hamLine[2].style.opacity = "1"
+    hamLine[1].style.transform = "rotate(0deg)"
+    counter = 0;
 }
+}
+
 
 //TASK 01-01-02
 const task010102 = document.getElementById("task1-box")
